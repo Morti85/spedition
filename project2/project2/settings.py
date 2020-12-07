ab5d4a156a148c5203b685a95a20e6a227b6786f
Tim Rössel <tim.roessel@gmx.de>
2020-12-07 15:47:59 +0100

    besser

--- project2/project2/settings.py
+++ project2/project2/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app1.apps.App1Config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- project2/project2/urls.py
+++ project2/project2/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('app1.urls')),
+    path('', include('app1.urls')),
 ]
