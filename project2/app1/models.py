d47b4588e55f09aaddf6c38349117866acb962e4
Tim Rössel <tim.roessel@gmx.de>
2020-12-09 09:24:16 +0100

    verbesserungen

--- project2/app1/models.py
+++ project2/app1/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Kunde(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name+" ("+self.id+")"
