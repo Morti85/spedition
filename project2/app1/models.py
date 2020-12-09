8da87bff4862e160c628781f577f8db6be94e00d
Tim Rössel <tim.roessel@gmx.de>
2020-12-09 11:07:32 +0100

    Datenbanken hinzugefügt und den Superuser angelegt

--- project2/app1/models.py
+++ project2/app1/models.py
@@ -1,13 +1,19 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class Kunde(models.Model):
     name = models.CharField(max_length=50)
     def __str__(self):
-        return self.name+" ("+self.id+")"
+        return self.name+" ("+str(self.id)+")"
+    class Meta:
+        verbose_name_plural = "Kunden"
 
 class Bestellung(models.Model):
-    kundennummer = models.Charfield(max_length=50)
-    produkte = models.CharField(max_length=50)
+    kunde = models.ForeignKey(Kunde, on_delete=models.CASCADE)
+    bestellung = models.TextField()
+    datum = models.DateTimeField(auto_now=False, auto_now_add=True)
     def __str__(self):
-        return self.kundennummer+" ("+self.id+")"
+        return self.kunde.name+", "+self.bestellung+", "+str(self.datum)
+    class Meta:
+        verbose_name_plural = "Bestellungen"
