a43f4e440def3838e4d7f52af56d9d7035fc498e
Tim Rössel <tim.roessel@gmx.de>
2020-12-09 09:52:24 +0100

    plopp

--- project2/app1/models.py
+++ project2/app1/models.py
@@ -5,3 +5,9 @@ class Kunde(models.Model):
     name = models.CharField(max_length=50)
     def __str__(self):
         return self.name+" ("+self.id+")"
+
+class Bestellung(models.Model):
+    kundennummer = models.Charfield(max_length=50)
+    produkte = models.CharField(max_length=50)
+    def __str__(self):
+        return self.kundennummer+" ("+self.id+")"
