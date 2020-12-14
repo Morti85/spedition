f331a1ef4eb0cd532d651257b5e8fc512ad313c4
Tim Rössel <tim.roessel@gmx.de>
2020-12-14 08:06:46 +0100

    db

--- project2/project2/settings.py
+++ project2/project2/settings.py
@@ -75,19 +75,17 @@ WSGI_APPLICATION = 'project2.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#       'NAME': BASE_DIR / 'db.sqlite3',
-#   }
-#}
-
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test2',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
+
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'test2',
+#        'USER': 'root',
+#        'PASSWORD': '',
+#        'HOST': 'localhost',
+#        'PORT': '3306',
     }
 }
 
